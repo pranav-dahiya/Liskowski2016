05c2df20690067ab930e686eceaf4b13859bef83
pranav-dahiya <pranav.dahiya15@gmail.com>
2019-06-06 14:42:57 +0530

    Added support for variable sized patches in Patch class

--- patch.py
+++ patch.py
@@ -2,11 +2,11 @@ import cv2
 import numpy as np
 
 class Patch:
-    def __init__(self,I,J,mask):
+    def __init__(self,I,J,mask,size):
         while True:
-            x = np.random.randint(584)
-            y = np.random.randint(565)
-            if mask[x,y] == 255:
+            x = np.random.randint(584-(size/2))
+            y = np.random.randint(565-(size/2))
+            if mask[x+(size/2),y+(size/2)] == 255:
                 break
-        self.data = I[x-13:x+14,y-13:y+14,:]
+        self.data = I[x-(size/2):x+(size-size/2),y-(size/2):y+(size-size/2),:]
         self.label = int(J[x,y]==255)
