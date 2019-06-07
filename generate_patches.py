76883990075de20ef569d4c4ca6ad5ac5328af6f
pranav-dahiya <pranav.dahiya15@gmail.com>
2019-06-07 15:09:27 +0530

    Fixed bug in patch generator

--- generate_patches.py
+++ generate_patches.py
@@ -6,17 +6,18 @@ x = np.zeros((400000,27,27,3))
 y = np.zeros((400000,2))
 
 for i in range(20):
-    I = cv2.imread('DRIVE/test/images/'+format(i+1,'02d')+'_test.tif')
-    J = cv2.imread('DRIVE/test/1st_manual/'+format(i+1,'02d')+'_manual1.tif',0)
-    mask = cv2.imread('DRIVE/test/mask/'+format(i+1,'02d')+'_test_mask.tif',0)
+    I = cv2.imread('DRIVE/training/images/'+format(i+21,'02d')+'_training.tif')
+    J = cv2.imread('DRIVE/training/1st_manual/'+format(i+21,'02d')+'_manual1.tif',0)
+    mask = cv2.imread('DRIVE/training/mask/'+format(i+21,'02d')+'_training_mask.tif',0)
     print(i)
     for j in range(20000):
+        index = i*20000+j
         try:
             patch = Patch(I,J,mask)
-            x[i*20+j] = patch.data
-            y[i*20+j,patch.label] = 1
+            x[index] = patch.data
+            y[index,patch.label] = 1
         except ValueError:
             j -= 1
 
-np.save('data/x_test.npy',x)
-np.save('data/y_test.npy',y)
+np.save('data/x_train.npy',x)
+np.save('data/y_train.npy',y)
