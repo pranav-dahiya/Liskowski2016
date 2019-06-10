8a2c70d45733df6ac21cdcba472cdc3e7f725ba5
pranav-dahiya <pranav.dahiya15@gmail.com>
2019-06-10 11:28:47 +0530

    Added support for generating larger datasets than available RAM

--- generate_patches.py
+++ generate_patches.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 from patch import Patch
 
-x = np.zeros((400000,27,27,3))
-y = np.zeros((400000,2))
+x = np.memmap('data/x_test.npy',dtype=np.uint8,mode='write',shape=(400000,27,27,3))
+y = np.memmap('data/y_test.npy',dtype=np.uint8,mode='write',shape=(400000,2))
 
 for i in range(20):
-    I = cv2.imread('DRIVE/training/images/'+format(i+21,'02d')+'_training.tif')
-    J = cv2.imread('DRIVE/training/1st_manual/'+format(i+21,'02d')+'_manual1.tif',0)
-    mask = cv2.imread('DRIVE/training/mask/'+format(i+21,'02d')+'_training_mask.tif',0)
+    I = cv2.imread('DRIVE/test/images/'+format(i+1,'02d')+'_test.tif')
+    J = cv2.imread('DRIVE/test/1st_manual/'+format(i+1,'02d')+'_manual1.tif',0)
+    mask = cv2.imread('DRIVE/test/mask/'+format(i+1,'02d')+'_test_mask.tif',0)
     print(i)
     for j in range(20000):
         index = i*20000+j
@@ -19,5 +19,5 @@ for i in range(20):
         except ValueError:
             j -= 1
 
-np.save('data/x_train.npy',x)
-np.save('data/y_train.npy',y)
+del x
+del y
